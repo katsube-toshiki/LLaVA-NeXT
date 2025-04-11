fa2bd015be239149f03e0871d423370872ce1a8a
katsube-toshiki <katsube@mi.t.u-tokyo.ac.jp>
2025-04-11 17:09:02 +0000

    train in singularity

--- scripts/train/pretrain_siglip.sh
+++ scripts/train/pretrain_siglip.sh
@@ -1,28 +1,38 @@
-export OMP_NUM_THREADS=8
-export NCCL_IB_DISABLE=0
+#!/bin/bash
+
+export OMP_NUM_THREADS=4
+# export NCCL_IB_DISABLE=0
 export NCCL_IB_GID_INDEX=3
-export NCCL_SOCKET_IFNAME=eth0
+# export NCCL_SOCKET_IFNAME=eth0
 export NCCL_DEBUG=INFO
 
-LLM_VERSION="Qwen/Qwen2-7B-Instruct"
-LLM_VERSION_CLEAN="${LLM_VERSION//\//_}"
+export NCCL_P2P_DISABLE=1
+export NCCL_SOCKET_IFNAME=enp225s0f1np1,enp226s0
+export NCCL_IB_DISABLE=1
+
+export NUM_GPUS=4
+export NNODES=1
+export RANK=0
+
+LLM_VERSION="llm-jp/llm-jp-3-13b-instruct3"
+LLM_VERSION_CLEAN="llm-jp-3-13b-instruct3"
 VISION_MODEL_VERSION="google/siglip-so400m-patch14-384"
-VISION_MODEL_VERSION_CLEAN="${VISION_MODEL_VERSION//\//_}"
+VISION_MODEL_VERSION_CLEAN="siglip-so400m-patch14-384"
 
 ############### Pretrain ################
 
 PROMPT_VERSION=plain
 
-BASE_RUN_NAME="llavanext-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-mlp2x_gelu-pretrain_blip558k_plain"
+BASE_RUN_NAME="llavanext-${VISION_MODEL_VERSION_CLEAN}-${LLM_VERSION_CLEAN}-mlp2x_gelu-pretrain_0223_filtered_plain"
 echo "BASE_RUN_NAME: ${BASE_RUN_NAME}"
 
-ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" --master_addr="${ADDR}" --master_port="${PORT}" \
-    llava/train/train_mem.py \
-    --deepspeed scripts/zero3.json \
+ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" \
+    /LLaVA-NeXT/llava/train/train_mem.py \
+    --deepspeed /LLaVA-NeXT/scripts/zero3.json \
     --model_name_or_path ${LLM_VERSION} \
     --version ${PROMPT_VERSION} \
-    --data_path /blip_558k/blip_558k_plain.json \
-    --image_folder /blip_558k/images \
+    --data_path /dataset/dataset_llm.json \
+    --image_folder /dataset/0223_filtered \
     --vision_tower ${VISION_MODEL_VERSION} \
     --mm_tunable_parts="mm_mlp_adapter" \
     --mm_vision_select_layer -2 \
@@ -30,7 +40,7 @@ ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NN
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir /checkpoints/projectors/${BASE_RUN_NAME} \
+    --output_dir /LLaVA-NeXT/checkpoints/projectors/${BASE_RUN_NAME} \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
